d71bbcd3f3026080471d9e9e3a062a24a90e09a3
Bnyro <bnyro@tutanota.com>
2023-06-26 10:38:07 +0200

    Add desktop file and icon to meson build script

--- src/config.rs
+++ src/config.rs
@@ -1,8 +1,3 @@
 pub const APP_ID: &str = "com.lemmy-gtk.lemoa";
-pub const GETTEXT_PACKAGE: &str = ;
-pub const LOCALEDIR: &str = ;
 pub const PKGDATADIR: &str = "/usr/local/share/lemoa";
-pub const PROFILE: &str = ;
-pub const RESOURCES_FILE: &str = concat!("/usr/local/share/lemoa", "/resources.gresource");
 pub const VERSION: &str = "0.1.0";
-
