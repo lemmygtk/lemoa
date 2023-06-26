7494340afb20f8cd97a76f4e31d4c76d8d302ead
Bnyro <bnyro@tutanota.com>
2023-06-26 13:32:03 +0200

    Add flatpak metadata and improve meson system

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,4 @@
 pub const NAME: &str = "lemoa";
-pub const APP_ID: &str = "com.lemmygtk.lemoa.Devel";
+pub const APP_ID: &str = "com.lemmygtk.lemoa";
 pub const PKGDATADIR: &str = "/app/share/lemoa";
-pub const VERSION: &str = "0.1.0-8e019361";
+pub const VERSION: &str = "0.1.0";
