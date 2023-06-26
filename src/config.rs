8e019361f48e2cdc77ea22a7b47ceea9bac0e398
Bnyro <bnyro@tutanota.com>
2023-06-26 11:38:25 +0200

    Fix issues with the meson build

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,3 @@
-pub const APP_ID: &str = "com.lemmy-gtk.lemoa";
+pub const APP_ID: &str = "com.lemmygtk.lemoa";
 pub const PKGDATADIR: &str = "/usr/local/share/lemoa";
 pub const VERSION: &str = "0.1.0";
